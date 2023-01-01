b3b617342d9a4b6b5c56789be8ff39c7537ffbcf
manoj <manojravula929@gmail.com>
2023-01-01 16:04:52 +0530

    read more func

--- src/pages/tours/Landingpage/LandingPage.js
+++ src/pages/tours/Landingpage/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getTours } from "../../../API/Tours";
 import Tour from "../tour/Tour";
-import img from "../../../images/img_missing.png";
 import Navbar from "../../../components/Navbar/Navbar";
 const LandingPage = () => {
   const [city, setCity] = useState([]);
@@ -13,20 +12,23 @@ const LandingPage = () => {
     setIsLoading(false);
   };
   const Loader = () => {
-    return isLoading && <div className="position-absolute top-50 start-50">
-        <div className="spinner-border" role='status'>
-          <span className="visually-hidden">loading.....</span>
+    return (
+      isLoading && (
+        <div className="position-absolute top-50 start-50">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">loading.....</span>
+          </div>
         </div>
-      </div>
+      )
+    );
   };
   useEffect(() => {
     getData();
   }, []);
   return (
     <div>
-        <Navbar/>
+      <Navbar />
       <div className="sm-container m-5">
-        <Tour/>
         <h1 className="display-1 text-center">OUR TOURS</h1>
         {Loader()}
         {!isLoading && (
@@ -35,17 +37,13 @@ const LandingPage = () => {
               {city.map((item) => {
                 return (
                   <>
-                    <div className="card shadow m-2">
-                      <img src={item.image} alt={img} className="" />
-                      <div className="card-body">
-                        <h5 className="card-title">{item.name}</h5>
-                        <h6 className="text-end text-danger card-title">
-                          $ {item.price} Only{" "}
-                        </h6>
-                        <p className="card-text">{item.info}</p>
-                        <button className="btn btn-text-danger btn-outline-danger">Not intersted</button>
-                      </div>
-                    </div>
+                    <Tour
+                      key={item.id}
+                      image={item.image}
+                      name={item.name}
+                      price={item.price}
+                      info={item.info}
+                    />
                   </>
                 );
               })}
--- src/pages/tours/tour/Tour.js
+++ src/pages/tours/tour/Tour.js
@@ -1,38 +1,32 @@
 import React, { useState } from "react";
+import img from "../../../images/img_missing.png";
 
 const Tour = ({ id, image, info, name, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
 
-  const ReadMore = () => {
-    setReadMore(true);
-  };
-  const ReadLess = () => {
-    setReadMore(false);
+  const ReadMore_ReadLess = () => {
+    !readMore ? setReadMore(true) : setReadMore(false);
   };
   return (
     <div>
-      <div className="card">
-        <img
-          src={
-            "https://images.ctfassets.net/hrltx12pl8hq/3Z1N8LpxtXNQhBD5EnIg8X/975e2497dc598bb64fde390592ae1133/spring-images-min.jpg?fit=fill&w=480&h=270"
-          }
-          alt={name}
-        />
+      <div className="card shadow m-5">
+        <img src={image} alt={img} />
         <footer>
           <div className="m-2">
-            <h4 className="card-title">Name</h4>
-            <h4 className="text-end text-danger">Price</h4>
+            <h4 className="card-title">{name}</h4>
+            <h4 className="text-end text-danger p-2">${price} only</h4>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti
-              exercitationem perspiciatis repellendus vitae earum tempora sint
-              labore molestias omnis dolorem saepe reiciendis eius, delectus
-              repellat neque. Iste laudantium laborum dolore!
-              <button className="btn text-info bg-light"
-              onClick={(!readMore)?ReadMore:ReadLess}>
-                {(!readMore) ? `read less` : `read more`}
+              {info}
+              <button
+                className="btn text-info bg-light"
+                onClick={ReadMore_ReadLess}
+              >
+                {!readMore ? `read less` : `read more`}
               </button>
             </p>
-            <button className="btn text-danger outline-danger">Not intrested</button>
+            <button className="btn text-danger outline-danger m-2">
+              Not intrested
+            </button>
           </div>
         </footer>
       </div>
